e081af2ecf708f14d2e8bbd846275d5d3b4521ec
upgle <admin@upgle.com>
2016-05-09 02:23:50 +0900

    template header, footer 분리.

---
+++ view/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome To TravelDiary App</title>
+</head>
+<body>
--- view/index.php
+++ view/index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome To TravelDiary App</title>
-</head>
-<body>
-    <h1><?=$content?></h1>
-</body>
-</html>
+<?php include "header.php" ?>
+<h1><?=$content?></h1>
+<?php include "footer.php" ?>
