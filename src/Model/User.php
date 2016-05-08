192f2b227050b9fc33b60ea1342eb7e721e3bfbc
upgle <admin@upgle.com>
2016-05-09 00:32:00 +0900

    User Model getter, setter 추가.

--- src/Model/User.php
+++ src/Model/User.php
@@ -33,4 +33,54 @@ class User
         $this->name = $name;
     }
 
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    
+
 }
