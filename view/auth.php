1293319a2eba080fd9dd820d6e6101d522acac04
upgle <admin@upgle.com>
2016-05-09 14:49:38 +0900

    auth.php include header, footer

--- view/auth.php
+++ view/auth.php
@@ -1,5 +1,9 @@
+<?php include "header.php" ?>
+
 <form action="/?r=/auth/login" method="post">
     아이디 : <input type="text" name="id" />
     패스워드 : <input type="password" name="password" />
     <input type="submit" value="로그인" />
 </form>
+
+<?php include "footer.php" ?>
