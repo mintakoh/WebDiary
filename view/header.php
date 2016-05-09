62c450ed405332bf758fb576e4aab61c87900160
upgle <admin@upgle.com>
2016-05-09 14:47:45 +0900

    header brand design 변경.

--- view/header.php
+++ view/header.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome To TravelDiary App</title>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/xeicon/1.0.4/xeicon.min.css">
+
+
+
     <style>
 
         * {
@@ -10,17 +14,25 @@
             margin: 0;
         }
 
+        body {
+            background-color: #f5f8fa;
+        }
+
         .container {
             margin: 0 auto;
             width: 900px;
         }
-
 
         #header {
             width: 100%;
             height: 60px;
             line-height: 60px;
             border-bottom: 1px solid #e1e8ed;
+            background: #fff;
+        }
+        #header .brand {
+            font-weight: normal;
+            font-size: 18px;
         }
 
         .secret_password{
@@ -35,7 +47,7 @@
 <body>
 <header id="header">
     <div class="container">
-    Travel Diary
+        <h1 class="brand"><i class="xi-flight"></i> Travel Diary</h1>
     </div>
 </header>
 <div class="container">
