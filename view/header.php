0f2ddbfaeb6803008b991342fb2d6823e4ac5491
upgle <admin@upgle.com>
2016-05-09 15:22:46 +0900

    프론트엔드 디자인 수정.

--- view/header.php
+++ view/header.php
@@ -3,53 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome To TravelDiary App</title>
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/xeicon/1.0.4/xeicon.min.css">
-
-
-
-    <style>
-
-        * {
-            padding: 0;
-            margin: 0;
-        }
-
-        body {
-            background-color: #f5f8fa;
-            font-size: 12px;
-        }
-
-        .pull-left {
-            float: left;
-        }
-        .pull-right {
-            float: right;
-        }
-        .container {
-            margin: 0 auto;
-            width: 900px;
-        }
-
-        #header {
-            width: 100%;
-            height: 60px;
-            line-height: 60px;
-            border-bottom: 1px solid #e1e8ed;
-            background: #fff;
-        }
-        #header .brand {
-            font-weight: normal;
-            font-size: 18px;
-        }
-
-        .secret_password{
-            display: none;
-        }
-
-        .check_secret:checked ~ .secret_password{
-            display: inline;
-        }
-    </style>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/xeicon/1.0.4/xeicon.min.css" />
+    <link rel="stylesheet" href="/resources/css/default.css" />
 </head>
 <body>
 <header id="header">
@@ -66,5 +21,4 @@
         </div>
     </div>
 </header>
-<div class="container">
 
--- view/index.php
+++ view/index.php
@@ -1,6 +1,19 @@
 <?php include "header.php" ?>
-<h1><?=$content?></h1>
 
-<a href="/?r=/auth">로그인</a>
-<a href="/?r=/article/write">글쓰기</a>
+
+<div id="visual">
+    <div class="container">
+        <div class="slogan">
+            <h3>행복했던 순간을 <strong>TRAVEL DIARY</strong>가 지켜드립니다.</h3>
+            <p class="start-now">지금 바로 시작하세요!</p>
+        </div>
+
+    </div>
+    <div class="cover"></div>
+</div>
+<div class="container">
+
+    <a href="/?r=/auth">로그인</a>
+    <a href="/?r=/article/write">글쓰기</a>
+</div>
 <?php include "footer.php" ?>
