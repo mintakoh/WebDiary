b4b3224e242322542bf5d7dd4a06fc796bb465ed
upgle <admin@upgle.com>
2016-05-09 15:33:57 +0900

    메인 visual 디자인 수정.

--- view/index.php
+++ view/index.php
@@ -6,14 +6,13 @@
         <div class="slogan">
             <h3>행복했던 순간을 <strong>TRAVEL DIARY</strong>가 지켜드립니다.</h3>
             <p class="start-now">지금 바로 시작하세요!</p>
+            <a class="btn-primary btn-large" href="/?r=/article/write">다이어리 새글쓰기</a>
         </div>
 
     </div>
     <div class="cover"></div>
 </div>
 <div class="container">
-
-    <a href="/?r=/auth">로그인</a>
-    <a href="/?r=/article/write">글쓰기</a>
+    .container
 </div>
 <?php include "footer.php" ?>
