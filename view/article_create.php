b65d7821af700189bdd89c80186f93d123e7b042
upgle <admin@upgle.com>
2016-05-11 11:28:17 +0900

    다이어리 새 글 쓰기 container 추가.

--- view/article_create.php
+++ view/article_create.php
@@ -1,20 +1,22 @@
 <?php include "header.php" ?>
-<form action="/?r=/article/create" method="post">
-    <div>
-        날짜 : <input type="date" name="date">
-    </div>
-    <div>
-        제목 : <input type="text" name="subject">
-    </div>
-    <div>
-        비밀글 : <input type="checkbox" class="check_secret">
-        <input type="password" name="secret" class="secret_password"><br/>
-    </div>
-    <div>
-        내용 : <textarea rows="10" cols="50" name="content"></textarea><br/>
-    </div>
-    <div>
-        <input type="submit" value="작성">
-    </div>
-</form>
+<div class="container">
+    <form action="/?r=/article/create" method="post">
+        <div>
+            날짜 : <input type="date" name="date">
+        </div>
+        <div>
+            제목 : <input type="text" name="subject">
+        </div>
+        <div>
+            비밀글 : <input type="checkbox" class="check_secret">
+            <input type="password" name="secret" class="secret_password"><br/>
+        </div>
+        <div>
+            내용 : <textarea rows="10" cols="50" name="content"></textarea><br/>
+        </div>
+        <div>
+            <input type="submit" value="작성">
+        </div>
+    </form>
+</div>
 <?php include "footer.php" ?>
