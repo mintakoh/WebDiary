edb30fc635c29cd272a133322cfd23c1834132b7
upgle <admin@upgle.com>
2016-05-12 12:46:14 +0900

    article_list 디자인 일부 수정.

--- view/article_list.php
+++ view/article_list.php
@@ -1,12 +1,16 @@
 <?php include "header.php" ?>
-<ul>
-    <?php
-    /** @var \Model\Article $article */
-    krsort($articles);
-    foreach($articles as $article) :
-    ?>
-        <li><a href="/?r=/article/<?=$article->getId()?>"><?=$article->getSubject()?></a>
-        <p><?=$article->getDate()." ".$article->getUser()->getName()?></p></li>
-    <?php endforeach; ?>
-</ul>
+<div class="container">
+    <ul class="article-list">
+        <?php
+        /** @var \Model\Article $article */
+        krsort($articles);
+        foreach($articles as $article) :
+        ?>
+            <li class="article-list-item">
+                <a href="/?r=/article/<?=$article->getId()?>" class="article-list-subject"><?=$article->getSubject()?></a>
+                <p><?=$article->getDate()." ".$article->getUser()->getName()?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
 <?php include "footer.php" ?>
