8423ce2d55dd9e7f561b42df2b119c3131925763
upgle <admin@upgle.com>
2016-05-17 01:24:58 +0900

    다이어리 글 보기 디자인 초안 작업.

--- view/article.php
+++ view/article.php
@@ -6,23 +6,33 @@
 ?>
 <div class="container">
 <?php if($article->getSecret() === "" || (isset($_POST["article_password"]) && $_POST["article_password"] === $article->getSecret())) { ?>
-    <p>작성일 : <?= $article->getDate(); ?></p>
-    <p>제목 : <?= $article->getSubject(); ?></p>
-    <p>작성자 : <?= $article->getUser()->getName(); ?></p>
-    <p>내용 : <?= $article->getContent(); ?></p>
-    <p>지출 내역</p>
-    <?php foreach($article->getReceipts() as $receipt) : ?>
-    <p>내용 : <?= $receipt->getSummary(); ?></p>
-    <p>통화 : <?= $receipt->getCurrency(); ?></p>
-    <p>가격 : <?= $receipt->getPrice(); ?></p>
-    <?php endforeach; ?>
-    <?php if($isOwner == true){ ?>
-    <a href="/?r=/articles/my">목록</a>
-    <a href="/?r=/article/modify/<?=$article->getId()?>">수정</a>
-    <a href="/?r=/article/remove/<?=$article->getId()?>">삭제</a>
-    <?php } else{ ?>
-    <a href="/?r=/user/<?=$article->getUser()->getId()?>/articles">목록</a>
-<?php }
+
+    <div class="article-view">
+        <h3 class="article-view-title"><?= $article->getSubject(); ?></h3>
+        <div class="article-view-meta">
+            <span><i class="xi-calendar"></i> <?= $article->getDate(); ?></span>
+            <span><i class="xi-user"></i> <?= $article->getUser()->getName(); ?></span>
+        </div>
+        <div class="article-view-content">
+            <?= $article->getContent(); ?>
+        </div>
+
+        <p>지출 내역</p>
+        <?php foreach($article->getReceipts() as $receipt) : ?>
+        <p>내용 : <?= $receipt->getSummary(); ?></p>
+        <p>통화 : <?= $receipt->getCurrency(); ?></p>
+        <p>가격 : <?= $receipt->getPrice(); ?></p>
+        <?php endforeach; ?>
+
+        <?php if($isOwner == true) : ?>
+        <a href="/?r=/articles/my">목록</a>
+        <a href="/?r=/article/modify/<?=$article->getId()?>">수정</a>
+        <a href="/?r=/article/remove/<?=$article->getId()?>">삭제</a>
+        <?php else : ?>
+        <a href="/?r=/user/<?=$article->getUser()->getId()?>/articles">목록</a>
+        <?php endif; ?>
+    </div>
+<?php
     } else{
 ?>
     
