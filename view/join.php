d810dc833906631776da456dabdd1673f8b2f49b
upgle <admin@upgle.com>
2016-05-17 01:07:37 +0900

    회원가입 required attribute 추가.

--- view/join.php
+++ view/join.php
@@ -4,19 +4,19 @@
         <h3 class="auth-form-title">JOIN</h3>
         <label for="id">
             아이디
-            <input type="text" name="id" />
+            <input type="text" name="id" required/>
         </label>
         <label for="name">
             이름
-            <input type="text" name="name" />
+            <input type="text" name="name" required/>
         </label>
         <label for="password">
             패스워드
-            <input type="password" name="password" />
+            <input type="password" name="password" required/>
         </label>
         <label for="password">
             패스워드 재확인
-            <input type="password" name="password2" />
+            <input type="password" name="password2" required/>
         </label>
         <input type="submit" value="회원가입" class="btn-primary" />
     </form>
