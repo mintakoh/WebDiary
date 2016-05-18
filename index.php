a00654e051cc994ae9669d78c001d96d9c889ee3
upgle <admin@upgle.com>
2016-05-19 06:16:08 +0900

    Session 클래스화.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 date_default_timezone_set("Asia/Seoul");
 
-session_start();
 require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/src/routes.php";
 require_once __DIR__."/src/helper.php";
@@ -14,6 +13,8 @@ $diaryRepository = new \Repository\ArticleFileRepository(__DIR__."/diary/", __DI
 /**
  * Run Application
  */
+Session::init();
+
 $app = new App($userRepository, $diaryRepository, $simpleTemplate);
 $app->setAsGlobal();
 $app->execute();
---
+++ src/Session.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: seonghyun
+ * Date: 2016. 5. 19.
+ * Time: 오전 6:09
+ */
+class Session
+{
+
+    private static $sessionStarted = false;
+
+
+    public static function init()
+    {
+        if (self::$sessionStarted == false) {
+            session_start();
+            self::$sessionStarted = true;
+        }
+    }
+
+
+    public static function exists($key)
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public static function set($key, $value = false)
+    {
+        if (is_array($key) && $value === false) {
+            foreach ($key as $name => $value) {
+                $_SESSION[$name] = $value;
+            }
+        } else {
+            $_SESSION[$key] = $value;
+        }
+    }
+
+    public static function pull($key)
+    {
+        if (isset($_SESSION[$key])) {
+            $value = $_SESSION[$key];
+            unset($_SESSION[$key]);
+            return $value;
+        }
+        return null;
+    }
+
+    public static function get($key, $child = false)
+    {
+        if ($child == true) {
+            if (isset($_SESSION[$key][$child])) {
+                return $_SESSION[$key][$child];
+            }
+        } else {
+            if (isset($_SESSION[$key])) {
+                return $_SESSION[$key];
+            }
+        }
+        return null;
+    }
+
+    public static function getAll()
+    {
+        return $_SESSION;
+    }
+
+
+    public static function destroy($key = '')
+    {
+        if (self::$sessionStarted == false) {
+            return;
+        }
+        if ($key =='') {
+            session_unset();
+            session_destroy();
+            return;
+        }
+        unset($_SESSION[$key]);
+    }
+}
