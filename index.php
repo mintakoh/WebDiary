c17bc387c26b946daedda7fc7eee1c2e4fc35542
upgle <admin@upgle.com>
2016-05-19 06:07:12 +0900

    helper, routes src 폴더 안으로 이동.

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ date_default_timezone_set("Asia/Seoul");
 
 session_start();
 require_once __DIR__."/vendor/autoload.php";
-require_once __DIR__."/app/routes.php";
-require_once __DIR__."/app/helper.php";
+require_once __DIR__."/src/routes.php";
+require_once __DIR__."/src/helper.php";
 
 $simpleTemplate = new \View\SimpleTemplate(__DIR__."/view");
 $userRepository = new \Repository\UserTextFileRepository(__DIR__."/storage/passwd.txt", $simpleTemplate);
@@ -17,3 +17,5 @@ $diaryRepository = new \Repository\ArticleFileRepository(__DIR__."/diary/", __DI
 $app = new App($userRepository, $diaryRepository, $simpleTemplate);
 $app->setAsGlobal();
 $app->execute();
+
+
