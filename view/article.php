c5725ac52eaf6b843dc4a15720241906592f9eba
upgle <admin@upgle.com>
2016-05-19 15:21:55 +0900

    글 보기 레이아웃 일부 수정.

--- view/article.php
+++ view/article.php
@@ -8,28 +8,48 @@
 <?php if($article->getSecret() === "" || (isset($_POST["article_password"]) && $_POST["article_password"] === $article->getSecret())) { ?>
 
     <div class="article-view">
-        <h3 class="article-view-title"><?= $article->getSubject(); ?></h3>
-        <div class="article-view-meta">
-            <span><i class="xi-calendar"></i> <?= $article->getDate(); ?></span>
-            <span><i class="xi-user"></i> <?= $article->getUser()->getName(); ?></span>
-        </div>
-        <div class="article-view-content">
-            <?= $article->getContent(); ?>
-        </div>
-        <p>지출 내역</p>
-        <?php foreach($article->getReceipts() as $receipt) : ?>
-        <p>내용 : <?= $receipt->getSummary(); ?></p>
-        <p>통화 : <?= $receipt->getCurrency(); ?></p>
-        <p>가격 : <?= $receipt->getPrice(); ?></p>
-        <?php endforeach; ?>
 
-        <?php if($isOwner == true) : ?>
-        <a href="/?r=/articles/my">목록</a>
-        <a href="/?r=/article/modify/<?=$article->getId()?>">수정</a>
-        <a href="/?r=/article/remove/<?=$article->getId()?>">삭제</a>
-        <?php else : ?>
-        <a href="/?r=/user/<?=$article->getUser()->getId()?>/articles">목록</a>
-        <?php endif; ?>
+        <div class="pull-left">
+            <h3 class="article-view-title"><?= $article->getSubject(); ?></h3>
+            <div class="article-view-meta">
+                <span><i class="xi-calendar"></i> <?= $article->getDate(); ?></span>
+                <span><i class="xi-user"></i> <?= $article->getUser()->getName(); ?></span>
+            </div>
+            <div class="article-view-content">
+                <?= $article->getContent(); ?>
+            </div>
+        </div>
+
+        <div class="pull-right">
+            <h3>지출 내역</h3>
+            <table class="expense-statement">
+                <colgroup>
+                    <col width="70%" />
+                </colgroup>
+                <thead>
+                    <th>항목</th>
+                    <th>가격</th>
+                </thead>
+                <tbody>
+                <?php foreach($article->getReceipts() as $receipt) : ?>
+                <tr class="expense-statement-item">
+                    <td><?=$receipt->getSummary()?></td>
+                    <td class="price"><?=$receipt->getCurrency()?> <?=number_format($receipt->getPrice())?></td>
+                </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="pull-right">
+    <?php if($isOwner == true) : ?>
+        <a href="/?r=/articles/my" class="btn-medium btn-primary">목록</a>
+        <a href="/?r=/article/modify/<?=$article->getId()?>" class="btn-medium btn-primary">수정</a>
+        <a href="/?r=/article/remove/<?=$article->getId()?>" class="btn-medium btn-primary">삭제</a>
+    <?php else : ?>
+        <a href="/?r=/user/<?=$article->getUser()->getId()?>/articles" class="btn-medium btn-primary">목록</a>
+    <?php endif; ?>
     </div>
 <?php
     } else{
